5b99b68e08dd207741fe08cf2c69c01b048dbe73
nimberledge <shadyemailid@gmail.com>
2017-07-24 07:17:12 +0530

    Add files via upload

---
+++ basegamestate.py
@@ -0,0 +1,106 @@
+import abc
+from abc import abstractmethod
+import time
+import random
+
+
+class BaseGameState(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, parent=None, agg_fn=max, children=None):
+        assert agg_fn in (max, min)
+        self.__parent = parent
+        if children:
+            self._children = children
+        else:
+            self._children = []
+        self.state_scores_cache = {}
+        self.agg_fn = agg_fn
+
+    @property
+    def parent(self):
+        """
+        :return: the parent game state of the current state
+        """
+        return self.__parent
+
+    @property
+    @abstractmethod
+    def children(self):
+        """
+        :return: list of game states immediately reachable from current state
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def static_score(self):
+        """
+        :return: evaluation function - customize to your game
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def __str__(self):
+        """
+        :return: string value of self
+        """
+        raise NotImplementedError
+
+    def dynamic_score(self, depth, termination_time=None):
+        """
+        :param depth: level of recursive depth to compute score
+        :param termination_time: time (as an absolute system time) passed to halt execution. optional parameter
+        :return: normalized minimax score of a state
+        """
+        assert depth >= 0
+        if depth in self.state_scores_cache:
+            return self.state_scores_cache[depth]
+
+        if depth == 0:
+            score = self.static_score
+            self.state_scores_cache[depth] = score
+            return score
+
+        if termination_time:
+            if time.time() >= termination_time:
+                return self.static_score
+
+        if not self.children:
+            return self.static_score
+
+        children_scores = []
+        for child in self.children:
+            if termination_time and time.time() >= termination_time:
+                children_scores.append(child.dynamic_score(0, termination_time=termination_time))
+                continue
+
+            children_scores.append(child.dynamic_score(depth - 1, termination_time=termination_time))
+
+        score = self.agg_fn(children_scores)
+        self.state_scores_cache[depth] = score
+        return score
+
+    def next_move(self, depth, termination_time=None):
+        """
+        :param depth: level of recursive depth to compute move
+        :param termination_time: time (as an absolute system time) passed to halt execution. optional parameter
+        :return: suggested child of state to play
+        """
+        score = self.dynamic_score(depth, termination_time=termination_time)
+
+        if not self.children:
+            return None
+        
+        for child in self.children:
+
+            if child.dynamic_score(depth-1, termination_time=termination_time) == score:
+                return child
+
+        # If it doesn't match any of its children
+        return self.children[random.randint(0, len(self.children)-1)]
+
+
+if __name__ == "__main__":
+    print 'Hellow, world! This is me using Python 2'
+
---
+++ blackholestate.py
@@ -0,0 +1,102 @@
+from basegamestate import BaseGameState
+from boardentry import BoardEntry
+
+class BlackHoleState(BaseGameState):
+
+    # Map of neighbours
+    N_MAP = {
+        1: (2, 3),
+        2: (1, 3, 4, 5),
+        3: (1, 2, 5, 6),
+        4: (2, 5, 7, 8),
+        5: (2, 3, 4, 6, 8, 9),
+        6: (3, 5, 9, 10),
+        7: (4, 8, 11, 12),
+        8: (4, 5, 7, 9, 12, 13),
+        9: (5, 6, 8, 10, 13, 14),
+        10: (6, 9, 14, 15),
+        11: (7, 12, 16, 17),
+        12: (7, 8, 11, 13, 17, 18),
+        13: (8, 9, 12, 14, 18, 19),
+        14: (9, 10, 13, 15, 19, 20),
+        15: (10, 14, 20, 21),
+        16: (11, 17),
+        17: (11, 12, 16, 18),
+        18: (17, 12, 13, 19),
+        19: (18, 13, 14, 20),
+        20: (19, 14, 15, 21),
+        21: (20, 15)
+    }
+
+    def __init__(self, state_list, parent=None, children=None):
+        self.state_list = state_list
+        agg_fn = (min, max)[sum([1 for index in range(len(self.state_list)) if self.state_list[index]]) % 2]
+        super(BlackHoleState, self).__init__(parent=parent, agg_fn=agg_fn, children=children)
+        self.string_rep = ''
+        self._static_score = None
+
+    @property
+    def children(self):
+        if self._children:
+            return self._children
+
+
+        moves_so_far = sum([1 for index in range(len(self.state_list)) if self.state_list[index]])
+
+        if moves_so_far >= 20:
+            self._children = []
+            return self._children
+
+        move_tuple = BoardEntry(moves_so_far//2 + 1, moves_so_far % 2)
+
+        temp_state = list(self.state_list)
+
+        for index in range(len(self.state_list)):
+            if self.state_list[index] is None:
+                temp_state[index] = move_tuple
+                child = BlackHoleState(temp_state)
+                self._children.append(child)
+                temp_state = list(self.state_list)
+
+        return self._children
+
+    @property
+    def static_score(self):
+        if self._static_score:
+            return self._static_score
+
+        sum_exposed_first_player = 0
+        sum_exposed_second_player = 0
+
+        for index in range (len(self.state_list)):
+            entry = self.state_list[index]
+            if entry is not None:
+
+                if entry.player == 0:
+                    for nbr in self.N_MAP[index+1]:
+                        if self.state_list[nbr-1] is None:
+                            sum_exposed_first_player += entry.value
+                            break
+
+                if entry.player == 1:
+                    for nbr in self.N_MAP[index+1]:
+                        if self.state_list[nbr-1] is None:
+                            sum_exposed_second_player += entry.value
+                            break
+
+        self._static_score = sum_exposed_first_player - sum_exposed_second_player
+        return self._static_score
+
+    @property
+    def state(self):
+        return self.state_list
+
+    def __str__(self):
+        if self.string_rep:
+            return self.string_rep
+        for ind, tup in enumerate(self.state_list):
+            self.string_rep += str(ind+1) + ': ' + str(tup).lstrip('BoardEntry') + '\n'
+        return self.string_rep
+
+if __name__ == "__main__":
+    pass
